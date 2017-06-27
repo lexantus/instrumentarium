eddeb5fd3207f3e3bffe821531294f660db6e0d4
Rozhin Alexey <alexey.rozhin90@gmail.com>
2017-06-28 02:35:26 +0300

    good open apps.html page

--- apps.js
+++ apps.js
@@ -6,13 +6,15 @@ function App(name, db, scripts, views, icon) {
   this.icon_folder = icon;
 }
 
-exports.getAppData = function(db, userId) {
+var sprintf = require("sprintf-js").sprintf;
+
+exports.getAppData = function(db, userId, callback) {
   var appsData = [];
 
-  var clms = 'name, db, scripts_folder';
+  var clms = 'name, db, scripts_folder, views_folder, icon';
   var tbl = 'user_apps';
-  var wr = 'id = ' + userId;
-  var qry = sprintf('SELECT %s FROM %s WHERE %s', c, t, w);
+  var wr = 'user_id = ' + userId;
+  var qry = sprintf('SELECT %s FROM %s WHERE %s', clms, tbl, wr);
 
   db.query(qry, function(err, rows, fields) {
     if (err) throw err;
@@ -20,8 +22,8 @@ exports.getAppData = function(db, userId) {
     var r;
     for (var i = 0; i < n; i++) {
       r = rows[i];
-      appData[i] = new App(r.name, r.db, r.scripts, r.views, r.icon);
+      appData[i] = new App(r.name, r.db, r.scripts_folder, r.views_folder, r.icon);
     }
   });
-  return appsData;
+  callback(appsData);
 };
