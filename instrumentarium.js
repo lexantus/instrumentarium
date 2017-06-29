87a8269cc1a5828cd90da626d634894c8a5ab1b1
Rozhin Alexey <alexey.rozhin90@gmail.com>
2017-06-29 03:17:54 +0300

    clean project

--- instrumentarium.js
+++ instrumentarium.js
@@ -1,15 +1,12 @@
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
-var url = require('url');
-var multer = require('multer');
 var mysql = require('mysql');
 var handlebars = require('express-handlebars').create({
   defaultLayout: 'main'
 });
 
 var app = express();
-var upload = multer();
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -25,40 +22,21 @@ var UserDB = require('./UserDB');
 var userDB = mysql.createConnection(UserDB.get());
 userDB.connect();
 
-var user;
-var apps;
-
 var my = require('./apps');
 
 app.get('/', function(req, res) {
-  if (!user) {
-    res.render('login');
-  } else {
-    my.getAppData(userDB, user.id, function(apps) {
-      res.render('apps');
-    });
-  }
+  res.render('login');
 });
 
 app.post('/api/login', function(req, res, next) {
   console.log("req body is " + JSON.stringify(req.body));
-  if (!user) {
-    Login.execute(userDB, req, res, function(user) {
-      if (user) {
-        my.getAppData(userDB, user.id, function(apps) {
-          res.render('apps');
-        });
-      }
-    });
-  } else {
-    res.send("You are already login");
-  }
-});
-
-app.get('/api/logout', function(req, res) {
-  user = undefined;
-  apps = undefined;
-  res.render('login');
+  Login.execute(userDB, req, res, function(user) {
+    if (user) {
+      my.getAppData(userDB, user.id, function(apps) {
+        res.render('apps');
+      });
+    }
+  });
 });
 
 app.listen(8001, '127.0.0.1');
