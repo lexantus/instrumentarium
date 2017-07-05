264be4a6f25672d753e9fa6db67c6f6957b10266
Rozhin Alexey <alexey.rozhin90@gmail.com>
2017-07-05 03:58:18 +0300

    start implement cookie for auth

--- instrumentarium.js
+++ instrumentarium.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var credentials = require('./credentials');
 var mysql = require('mysql');
 var handlebars = require('express-handlebars').create({
   defaultLayout: 'main'
@@ -16,6 +18,13 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(cookieParser(credentials.cookieSecret, {
+  maxAge: 20000,
+  secure: true,
+  httpOnly: true,
+  signed: true
+}));
+
 var Login = require('./Login');
 var UserDB = require('./UserDB');
 
@@ -25,14 +34,28 @@ userDB.connect();
 var my = require('./apps');
 
 app.get('/', function(req, res) {
-  res.render('login');
+  if (req.signedCookies.session_id) {
+    my.getAppDataBySessionId(userDB, req.signedCookies.session_id, function(apps) {
+      res.render('apps');
+    });
+  } else {
+    res.render('login');
+  }
 });
 
 app.post('/api/login', function(req, res, next) {
   console.log("req body is " + JSON.stringify(req.body));
+  if (req.signedCookies.session_id) {
+    res.send("You are already login");
+    next();
+  }
+
   Login.execute(userDB, req, res, function(user) {
     if (user) {
       my.getAppData(userDB, user.id, function(apps) {
+        res.cookie('session_id', user.session_id, {
+          signed: true
+        });
         res.render('apps');
       });
     }
