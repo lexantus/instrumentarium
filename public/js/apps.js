ec023c8d8a4a7e3adc2582e4cafe5a0193e7c471
root <root@instrumentarium.club>
2017-10-15 01:51:03 +0300

    closure (passing data in event handlers)

--- public/js/apps.js
+++ public/js/apps.js
@@ -1,25 +1,34 @@
 (function () {
-  let div = document.getElementsByClassName("apps")[0];
+  var div = document.getElementsByClassName("apps")[0];
 
   function loadApp(json) {
     console.log(json);
   }
 
-  let xhrs = [];
-  let xhr;
-  for (let i = 0; i < div.children.length; i++) {
+  function xhrStateHandler(i) {
+       function checkXHR()
+       { 
+          if (xhrs[i].status === 200 && xhrs[i].readyState === 4) {
+             div.children[i].style.visibility = false;
+             loadApp(JSON.parse(xhrs[i].responseText));
+          }
+       }
+       return checkXHR;
+  }
+
+  function clickHandler(i) {
+	function sendXHR() {
+	  div.children[i].classList.add('icon-loader');
+          xhrs[i].send();
+	}
+	return sendXHR;
+  }
+
+  var xhrs = [];
+  for (var i = 0; i < div.children.length; i++) {
     xhrs.push(new XMLHttpRequest());
-    xhr = xhrs[xhrs.length - 1];
-    xhr.open('GET', 'ajax/' + div.children[i].dataset.appname, true);
-    xhr.onreadystatechange = function () {
-      if (xhr.status === 200 && xhr.readyState === 4) {
-        div.children[i].style.visibility = false;
-        loadApp(JSON.parse(xhr.responseText));
-      }
-    };
-    div.children[i].addEventListener('click', function () {
-      div.children[i].classList.add('icon-loader');
-      xhrs[i].send();
-    });
+    xhrs[xhrs.length - 1].open('GET', 'ajax/' + div.children[i].dataset.appname, true);
+    xhrs[xhrs.length - 1].onreadystatechange = xhrStateHandler(i);
+    div.children[i].addEventListener('click', clickHandler(i));
   }
 })();
