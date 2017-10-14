ee589641be8089107e6f070353f52f0b13a2a02b
alex <alexey.rozhin90@gmail.com>
2017-10-14 22:54:39 +0300

    try to understand this in handler in readystatechange event

--- public/js/apps.js
+++ public/js/apps.js
@@ -5,20 +5,20 @@
     console.log(json);
   }
 
-  let xhrs = [];
-  let xhr;
-  for (let i = 0; i < div.children.length; i++) {
+  var xhrs = [];
+  var xhr;
+  for (var i = 0; i < div.children.length; i++) {
     xhrs.push(new XMLHttpRequest());
     xhr = xhrs[xhrs.length - 1];
     xhr.open('GET', 'ajax/' + div.children[i].dataset.appname, true);
     xhr.onreadystatechange = function () {
-      if (xhr.status === 200 && xhr.readyState === 4) {
+      if (this.status === 200 && this.readyState === 4) {
         div.children[i].style.visibility = false;
         loadApp(JSON.parse(xhr.responseText));
       }
     };
     div.children[i].addEventListener('click', function () {
-      div.children[i].classList.add('icon-loader');
+      this.classList.add('icon-loader');
       xhrs[i].send();
     });
   }
