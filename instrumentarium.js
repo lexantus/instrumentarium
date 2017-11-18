ffcedff796b07e059c0703e88bc0786a982925ef
alex <alexey.rozhin90@gmail.com>
2017-11-19 02:55:42 +0300

    www to non www

--- instrumentarium.js
+++ instrumentarium.js
@@ -34,6 +34,17 @@ app.use(function (req, res, next) {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+function wwwRedirect(req, res, next) {
+  if (req.headers.host.slice(0, 4) === 'www.') {
+    let newHost = req.headers.host.slice(4);
+    return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+  }
+  next();
+};
+
+app.set('trust proxy', true);
+app.use(wwwRedirect);
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
